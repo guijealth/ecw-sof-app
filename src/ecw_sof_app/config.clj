a33ff4ebd53a990e58b16fdcaa494ae7d71d736b
Guillermo <guillerglez88@gmail.com>
2025-03-10 15:52:56 +0100

    adding required deps

---
+++ src/ecw_sof_app/config.clj
@@ -0,0 +1,17 @@
+(ns ecw-sof-app.config
+  (:require
+   [config.dotenv :as dotenv]
+   [config.sysenv :as sysenv]
+   [config.core :as cfg]))
+
+(def struc
+  [[:ecw
+    [:client
+     [:id {:env "ECW_CLIENT_ID", :of-type :cfg/string}]
+     [:secret {:env "ECW_CLIENT_SECRET", :of-type :cfg/string}]]]])
+
+(def config
+  (delay
+    (-> {}
+        (cfg/patch (dotenv/parse ".env") struc)
+        (cfg/patch (sysenv/read-all) struc))))
